213c1d01cdc28cbf69eb2acde0a81e2c8e41f084
diegobkabum <diego.benedetti@kabum.com.br>
2026-07-24 13:45:44 -0300

    feat: integração melhor envio

--- src/types/index.ts
+++ src/types/index.ts
@@ -138,6 +138,7 @@ export interface CompanySettings {
   freteCEPOrigem?: string | null;
   fretePesoDefaultGramas: number;
   melhorEnvioToken?: string | null;
+  melhorEnvioApiToken?: string | null;
   fretePacoteAltura: number;
   fretePacoteLargura: number;
   fretePacoteComprimento: number;
